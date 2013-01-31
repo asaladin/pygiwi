611a4bec7ce50a9bcecd0b9ce55d138cd50e08e9
Adrien Saladin <adrien.saladin@gmail.com>
2013-01-31 13:00:36 +0100

    define root git repositories in .ini file

--- pygiwi/views.py
+++ pygiwi/views.py
@@ -4,24 +4,29 @@ from lib import renderers
 
 import glob
 import os
+import os.path
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'pygiwi'}
 
-
+#this view returns the wiki page content
 @view_config(route_name="view_wiki", renderer="pygiwi:templates/wiki.mako")
 def view_wiki(request):
     pagename = request.matchdict["page"]
     project = request.matchdict["project"]
     
-    files = glob.glob("/home/saladin/Src/Web/Pyramid/testwiki/%s.*"%pagename)
-    print "files:", files
+    #construction of the wiki path
+    wikiroot = request.registry.settings['wiki.root']  #from settings in .ini file.
+    wikipath = os.path.join(wikiroot, project) #project name is the name of the git repository
+    rootfilepath = os.path.join(wikipath, pagename) #we want one specific file into this directory
+    
+    files = glob.glob(rootfilepath+".*")  #list files with any extension
     f = files[0]   #we take the first matching file, undertermined results if two files only differs by extension
+
     ext = os.path.splitext(f)[1]
     content = open(f, "r").read()
 
-    html = renderers[ext](content)
-    
+    html = renderers[ext](content) #selection of the correct renderer from the file extension
     
     return {"wikis": ["un", "deux"], "content": html}
